0c25477dd0b84397be93659c49a7c1f6713723aa
chieri-sato <chieri-sato@example.com>
2022-05-13 05:49:48 +0000

    ページネーションを書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,29 +10,35 @@ use App\Models\Task as TaskModel;
 class TaskController extends Controller
 {
     /**
-     * トップページ を表示する
+     * タスク一覧ページ を表示する
      * 
      * @return \Illuminate\View\View
      */
     public function list()
     {
+        // 1Page辺りの表示アイテム数を設定
+        $per_page = 15;
+        
         // 一覧の取得
         $list = TaskModel::where('user_id', Auth::id())
                          ->orderBy('priority', 'DESC')
                          ->orderBy('period')
                          ->orderBy('created_at')
-                         ->get();
+                         ->paginate($per_page);
+                        // ->get();
+/*
 $sql = TaskModel::where('user_id', Auth::id())
                  ->orderBy('priority', 'DESC')
                  ->orderBy('period')
                  ->orderBy('created_at')
                  ->toSql();
 //echo "<pre>\n"; var_dump($sql, $list); exit;
-//var_dump($sql);
-
+var_dump($sql);
+*/
         //
         return view('task.list', ['list' => $list]);
     }
+
     /**
      * タスクの新規登録
      */
